3fec103c0c889ad5bab9634b61cebab460f96555
Nestor Debiesme <nedebies@student.s19.be>
2023-05-16 10:09:08 +0200

    ajout de testeur de path

--- src/char_checker.c
+++ src/char_checker.c
@@ -6,67 +6,32 @@
 /*   By: nesdebie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 20:06:05 by nedebies          #+#    #+#             */
-/*   Updated: 2023/05/15 13:08:54 by nesdebie         ###   ########.fr       */
+/*   Updated: 2023/05/16 10:08:32 by nesdebie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-static int pathchecker(t_game *game, size_t xy, size_t prev_xy)
+int	check_path(t_game *game, int y, int x, int exit)
 {
-	size_t	i;
-	size_t	j;
 
-	i = xy / game->maps.cols;
-	j = xy % game->maps.cols;
-	if (game->maps.coord[i][j] != '1' && game->maps.coord[i][j] != 'E')
+	if (game->maps.coord[y][x] == '1')
+		return (0);
+	if (game->maps.coord[y][x] == 'C')
+		game->maps.cnt.collectible--;
+	if (game->maps.coord[y][x] == 'E')
 	{
-		if (prev_xy != 0 && game->maps.coord[i][j] == 'P')
-			return (1);
-		if (game->maps.coord[i - 1][j] && (i - 1) * j != prev_xy && i - 1 > 0)
-			return(pathchecker(game, (i - 1) * j, xy));
-		if (game->maps.coord[i + 1][j] && (i + 1) * j != prev_xy && i + 1 < (size_t)game->maps.cols)
-			return(pathchecker(game, (i + 1) * j, xy));
-		if (game->maps.coord[i][j - 1] && i * (j - 1) != prev_xy && j - 1 > 0)
-			return(pathchecker(game, i * (j - 1), xy));
-		if (game->maps.coord[i][j + 1] && i * (j + 1) != prev_xy && j + 1 < (size_t)game->maps.rows)
-			return(pathchecker(game, i  * (j + 1), xy));
+		exit = 1;
+		return (0);
 	}
-	return (pathchecker(game, prev_xy, xy));
-}
-
-#include <stdio.h>
-
-static int	is_reachable(t_game *game, int nb, char c)
-{
-	size_t i;
-	size_t j;
-
-	i = 0;
-	j = 0;
-	printf("%i\n", nb);
-	while (game->maps.coord[i][j] && nb != 0)
-	{
-		printf("ici\n");
-		j = 0;
-		while (game->maps.coord[i][j] && nb != 0)
-		{
-			printf("ici2\n");
-			if (game->maps.coord[i][j] == c)
-			{
-				printf("ici3\n");
-				nb--;
-				if (pathchecker(game, i * j, 0) && c == 'E')
-					return (1);
-				if (!pathchecker(game, i * j, 0))
-					return (0);
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("%i\n", nb);
-	if (nb == 0)
+	game->maps.coord[y][x] = '1';
+	if (check_path(game, y + 1, x, exit))
+		return (1);
+	if (check_path(game, y - 1, x, exit))
+		return (1);
+	if (check_path(game, y, x + 1, exit))
+		return (1);
+	if (check_path(game, y, x - 1, exit))
 		return (1);
 	return (0);
 }
@@ -78,20 +43,27 @@ static void	init_compo_cnt(t_game *game)
 	game->maps.cnt.collectible = 0;
 }
 
-static void	check_compo_cnt(t_game *game)
+static void	check_compo_cnt(t_game *game, int x, int y)
 {
+	int	valid_exit;
+	int	nb_collec;
+
+	valid_exit = 0;
+	nb_collec = game->maps.cnt.collectible;
 	if (game->maps.cnt.player < 1)
 		ft_error("One player is required.");
 	if (game->maps.cnt.player > 1)
 		ft_error("Too many players (max. 1).");
 	if (game->maps.cnt.collectible < 1)
 		ft_error("One collectible is required.");
-	if (!is_reachable(game, game->maps.cnt.collectible, 'C'))
-		ft_error("All collectibles are not reachable.");
 	if (game->maps.cnt.exit < 1)
 		ft_error("One exit is required.");
-	if (!is_reachable(game, game->maps.cnt.exit, 'E'))
-		ft_error("Exit not reachable.");
+	if (game->maps.cnt.exit > 1)
+		ft_error("Too many exits.");
+	check_path(game, y, x, &valid_exit);
+	if (!(valid_exit == 1 && game->maps.cnt.collectible == 0))
+		ft_error("No valid path");
+	game->maps.cnt.collectible = nb_collec;
 }
 
 static void	get_compo_coord2(t_game *game, int i, int j)
@@ -117,7 +89,11 @@ void	get_compo_coord(t_game *game)
 {
 	int	i;
 	int	j;
+	int x_p;
+	int y_p;
 
+	x_p = 0;
+	y_p = 0;
 	init_compo_cnt(game);
 	i = 0;
 	while (i < game->maps.rows)
@@ -125,10 +101,15 @@ void	get_compo_coord(t_game *game)
 		j = 0;
 		while (j < game->maps.cols)
 		{
+			if (game->maps.coord[i][j] == 'P')
+			{
+				x_p = j;
+				y_p = i;
+			}
 			get_compo_coord2(game, i, j);
 			j++;
 		}
 		i++;
 	}
-	check_compo_cnt(game);
+	check_compo_cnt(game, x_p, y_p);
 }
